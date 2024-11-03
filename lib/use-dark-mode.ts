09084236346839085d7c98714fa1c76f5beb4bbe
Prajwal Rangegowda <147972135+PrajwalRangegowda@users.noreply.github.com>
2024-11-03 13:17:38 +0530

    reverting back to the previous changes

--- lib/use-dark-mode.ts
+++ lib/use-dark-mode.ts
@@ -1,7 +1,7 @@
 import useDarkModeImpl from '@fisch0920/use-dark-mode'
 
 export function useDarkMode() {
-  const darkMode = useDarkModeImpl(false, { classNameDark: '' })
+  const darkMode = useDarkModeImpl(false, { classNameDark: 'dark-mode' })
 
   return {
     isDarkMode: darkMode.value,
